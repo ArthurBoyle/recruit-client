c356b6fbc6b0297cf4c81831bf9243118640f42a
ArthurBoyle <75610914+ArthurBoyle@users.noreply.github.com>
2021-02-16 14:34:20 +0800

    feat：创建静态组件

---
+++ src/containers/register/index.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                Register
+            </div>
+        );
+    }
+}
+
+export default connect(
+    state => ({}),
+    {}
+)(Register);
