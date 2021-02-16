80385d9b3ad05ff4dd858af56ea2fda48c8d3d79
ArthurBoyle <75610914+ArthurBoyle@users.noreply.github.com>
2021-02-16 16:56:21 +0800

    feat：更新Login组件静态页面，使其可以收集表单数据

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,12 +1,40 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
+import {NavBar, WingBlank, List, InputItem, Button, WhiteSpace} from "antd-mobile";
+import Logo from "../../components/logo";
 
 class Login extends Component {
+    state = {
+        username: "",
+        password: ""
+    };
+    handleChange = (name, value) => {
+        this.setState({
+            [name]: value
+        })
+    }
+    login = () => {
+        console.log(this.state)
+    }
+    toRegister = () => {
+        this.props.history.replace("/register");
+    }
     render() {
         return (
-            <div>
-                Login
-            </div>
+            <Fragment>
+                <NavBar>BOSS直聘</NavBar>
+                <Logo/>
+                <WingBlank>
+                    <List>
+                        <InputItem onChange={value => this.handleChange("username", value)}>用户名：</InputItem>
+                        <InputItem type="password" onChange={value => this.handleChange("password", value)}>密码：</InputItem>
+                    </List>
+                    <WhiteSpace/>
+                    <Button type="primary" onClick={this.login}>登录</Button>
+                    <WhiteSpace/>
+                    <Button onClick={this.toRegister}>还没有账户</Button>
+                </WingBlank>
+            </Fragment>
         );
     }
 }
